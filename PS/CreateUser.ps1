da416f579cf3483ea26f305ac644f5f57e34a982
schettn <nicoschett@icloud.com>
2019-09-25 16:38:24 +0200

    Optimize script
    Script is called relativ now. Exception handling added.

--- PS/CreateUser.ps1
+++ PS/CreateUser.ps1
@@ -1,22 +1,26 @@
-﻿$CSVFile = "..\CSV\users.csv"
-
-$UserList = Import-Csv -Path $CSVFile -Delimiter ";"
+﻿$CSVFile = "$PSScriptRoot\..\CSV\users.csv"
+$UserList = Import-Csv $CSVFile -Delimiter ";"
 
 ForEach ($User in $UserList)
 {
-    $Username = $User.Username
-    $Password = $User.Password
-    $Firstname = $User.Firstname
-    $Lastname = $User.Lastname
-    $Department = $User.Department
-    $Path = $User.Path
-    $Group = $User.Group
+    try{
+        $Username = $User.Username
+        $Password = $User.Password
+        $Firstname = $User.Firstname
+        $Lastname = $User.Lastname
+        $Department = $User.Department
+        $Path = $User.Path
+        $Group = $User.Group
 
-    Write-Host -ForegroundColor Yellow "Creating: $Username"
-    New-ADUser -Name "$Firstname $Lastname" -SamAccountName $Username -UserPrincipalName "$Username@pinterid.local" -GivenName $Firstname -Surname $Lastname -Enabled $true -ChangePasswordAtLogon $true -DisplayName "$Lastname $Firstname" -Department $Department -Path $Path -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
-    if ($Group)
-    {
-       Add-ADGroupMember -Identity $Group -Members $Username
+        Write-Host -ForegroundColor Yellow "Creating: $Username"
+        New-ADUser -Name "$Firstname $Lastname" -SamAccountName $Username -UserPrincipalName "$Username@nwtk.local" -GivenName $Firstname -Surname $Lastname -Enabled $true -ChangePasswordAtLogon $true -DisplayName "$Lastname $Firstname" -Department $Department -Path $Path -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force)
+        if ($Group)
+        {
+        Add-ADGroupMember -Identity $Group -Members $Username
+        }
+        Write-Host -ForegroundColor Green "Created: $Username"
+
+    }catch{
+        Write-Host -ForegroundColor red $error[0].Exception.Message
     }
-    Write-Host -ForegroundColor Green "Created: $Username"
 }
